af05479525047c5b1c846580a29480863ab61cd4
Kalai <skl.rsn@gmail.com>
2021-03-31 00:09:06 +0300

    Add dynamic instance name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "openstack_compute_instance_v2" "basic" {
-  name            = "basic"
+  name            = uuid()
   image_id        = "da1965db-174b-4433-b511-6a72c36e3fa1"
   flavor_id       = "3"
   key_pair        = var.openstack_keypair_name
@@ -10,5 +10,5 @@ resource "openstack_compute_instance_v2" "basic" {
   }
 
   user_data = file("init.sh")
-  
+
 }
