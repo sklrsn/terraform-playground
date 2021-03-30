ba30fef303e09b2fb32e1ff15fa7e248820bebcd
Kalai <skl.rsn@gmail.com>
2021-03-30 23:15:18 +0300

    fix image_id

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "openstack_compute_instance_v2" "basic" {
   name            = "basic"
-  image_id        = "ad091b52-742f-469e-8f3c-fd81cadf0743"
+  image_id        = "da1965db-174b-4433-b511-6a72c36e3fa1"
   flavor_id       = "3"
   key_pair        = "${var.openstack_keypair_name}"
   security_groups = ["default"]
